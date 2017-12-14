9fed197a4fc4dea28662582cdd49029b58867e99
Scruel Tao <scruelt@hotmail.com>
2017-12-14 08:46:13 +0800

    optimize ClipboardUtils, merge paste action to PasteAction.

--- src/main/java/pers/scruel/util/ClipboardUtils.java
+++ src/main/java/pers/scruel/util/ClipboardUtils.java
@@ -1,31 +1,22 @@
 package pers.scruel.util;
 
-import org.jsoup.helper.StringUtil;
-
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
-import java.io.IOException;
 
 /**
  * @author Scruel Tao <scruel@vip.qq.com>
  */
 public class ClipboardUtils {
   public static void setClipBoard(String text) {
-    if (StringUtil.isBlank(text)) {
+    if (StringUtils.isBlank(text)) {
       return;
     }
     Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
     StringSelection selection = new StringSelection(text);
     clipboard.setContents(selection, selection);
-    try {
-      if (!"false".equals(PropertiesUtils.getProperties().getProperty("auto.paste"))) {
-        paste();
-      }
-    } catch (IOException ignore) {
-    }
   }
 
   public static void paste() {
